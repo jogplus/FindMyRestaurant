7ec1c20d240dbc82d357e3733627d23677fda41a
Zachary Pinto <zpinto@uci.edu>
2020-03-07 19:54:30 -0800

    Add the square client class

--- TheBestApp/Helpers/SquareClient.swift
+++ TheBestApp/Helpers/SquareClient.swift
@@ -12,42 +12,9 @@ class SquareClient {
     static var CLIENT_ID = "QA1L0Z0ZNA2QVEEDHFPQWK0I5F1DE3GPLSNW4BZEBGJXUCFL"
     static var CLIENT_SECRET = "W2AOE1TYC4MHK5SZYOUGX0J3LVRALMPB4CXT3ZH21ZCPUMCU"
     
-    static var categoryList:NSArray = []
-    
     static func fetchCategories() {
-        let baseUrlString = "https://api.foursquare.com/v2/venues/categories?"
-        let queryString = "client_id=\(SquareClient.CLIENT_ID)&client_secret=\(SquareClient.CLIENT_SECRET)&v=20141020&id=4d4b7105d754a06374d81259"
-
-        let url = URL(string: baseUrlString + queryString.addingPercentEncoding(withAllowedCharacters: CharacterSet.urlQueryAllowed)!)!
-        
-        print(url)
-        let request = URLRequest(url: url)
-
-        let session = URLSession(
-            configuration: URLSessionConfiguration.default,
-            delegate:nil,
-            delegateQueue:OperationQueue.main
-        )
-        
-        let task : URLSessionDataTask = session.dataTask(with: request,
-            completionHandler: { (dataOrNil, response, error) in
-                if let data = dataOrNil {
-                    if let responseDictionary = try! JSONSerialization.jsonObject(
-                        with: data, options:[]) as? NSDictionary {
-                            NSLog("response: \(responseDictionary)")
-                        SquareClient.categoryList = responseDictionary.value(forKeyPath: "response.categories") as! NSArray
-//                            self.tableView.reloadData()
-                        print(SquareClient.categoryList)
-
-                    }
-                }
-        });
-        task.resume()
-    }
-    
-    func fetchLocations(_ query: String, near: String = "Irvine") {
         let baseUrlString = "https://api.foursquare.com/v2/venues/search?"
-        let queryString = "client_id=\(SquareClient.CLIENT_ID)&client_secret=\(SquareClient.CLIENT_SECRET)&v=20141020&near=\(near),CA&query=\(query)"
+        let queryString = "client_id=\(SquareClient.CLIENT_ID)&client_secret=\(SquareClient.CLIENT_SECRET)&v=20141020&near=Irvine,CA&query=restaurant"
 
         let url = URL(string: baseUrlString + queryString.addingPercentEncoding(withAllowedCharacters: CharacterSet.urlQueryAllowed)!)!
         let request = URLRequest(url: url)
@@ -64,9 +31,17 @@ class SquareClient {
                     if let responseDictionary = try! JSONSerialization.jsonObject(
                         with: data, options:[]) as? NSDictionary {
                             NSLog("response: \(responseDictionary)")
-//                            self.results = responseDictionary.value(forKeyPath: "response.venues") as! NSArray
-//                            self.tableView.reloadData()
-
+                        let restaurants = responseDictionary.value(forKeyPath: "response.venues") as! NSArray
+                        
+                        var categoriesSet = Set<String>()
+                        for restaurant in restaurants as! [NSDictionary] {
+                            
+                            let categories = restaurant.value(forKeyPath: "categories")
+                            
+                            for category in categories as! [NSDictionary] {
+                                categoriesSet.insert(category.value(forKeyPath: "name") as! String)
+                            }
+                        }
                     }
                 }
         });
